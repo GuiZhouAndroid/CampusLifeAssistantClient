f0a417ec6210071b0adada03d248cb3ea12fb7a4
GuiZhouAndroid <2393579405@qqcom>
2021-08-12 02:54:52 +0800

    登录界面

---
+++ app/src/main/java/work/lpssfxy/www/campuslifeassistantclient/utils/dialog/LoadingDialog.java
@@ -0,0 +1,44 @@
+package work.lpssfxy.www.campuslifeassistantclient.utils.dialog;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.view.Window;
+import android.widget.Toast;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import work.lpssfxy.www.campuslifeassistantclient.R;
+
+
+public class LoadingDialog {
+
+	public static Dialog dialog;
+	public static Date data;
+	public static SimpleDateFormat format;
+	
+
+	public static void showToast(Context context, String msg){
+		Toast.makeText(context,msg, 1).show();
+	}
+	
+	public static void showDialog(Context context){
+		dialog=new Dialog(context, R.style.dialog);
+		dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+		dialog.setContentView(R.layout.login_dialog);
+		dialog.setCanceledOnTouchOutside(false);
+		dialog.show();
+	}
+	public static void disDialog(){
+		dialog.dismiss();
+	}
+	
+	public static String getTime(){
+		data=new Date(System.currentTimeMillis());
+		format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String string=format.format(data);
+		return string;
+	}
+}
